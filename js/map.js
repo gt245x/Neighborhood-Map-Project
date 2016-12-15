f44a366933c6a56371ffb21e153df79f9f8b52fc
gt245x <dchuckse@yahoo.com>
2016-12-14 21:07:24 -0600

    Fixed clicked function for locations

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var map;
 var allLatlng = [];
 var markerlist = [];
-var resultList = []
+var resultList = [];
 var infowindow;
 var pos;
 var userCords;
@@ -14,7 +14,7 @@ var menu = ko.observable("open");
 var atlantaLocations = [
         {
             location : {lat: 33.772620, lng : -84.385561},
-            name : "The Fox theatre"
+            name : "The Fox Theatre"
         },
         {
             location : {lat : 33.762742, lng : -84.392664},
@@ -22,19 +22,19 @@ var atlantaLocations = [
         },
         {
             location : {lat : 33.763424, lng : -84.394891},
-            name : "Georgia Aquarium",
+            name : "Georgia Aquarium"
         },
         {
             location : {lat : 33.757168, lng : -84.396345},
-            name : "Philips Arena",
+            name : "Philips Arena"
         },
         {
             location : {lat : 33.732162, lng : -84.371335},
-            name : "Zoo Atlanta",
+            name : "Zoo Atlanta"
         },
         {
             location : {lat : 33.771510, lng : -84.389311},
-            name : "The Varsity",
+            name : "The Varsity"
         }
 ];
 
@@ -45,12 +45,10 @@ var locations = function(data, index) {
     this.location = data.location;
     this.desc = data.desc;
     this.markerindex = ko.observable(index);
+    };
 
 
-}
-
-
-var walmartdata = function(data) {
+var walmartData = function(data) {
     var self = this;
     this.title = ko.observable(data.title);
     this.add = ko.observable(data.address);
@@ -60,13 +58,10 @@ var walmartdata = function(data) {
     this.phone=ko.observable(data.phone);
     this.openSundays=ko.observable(data.openSundays);
     this.id = ko.observable(data.id);
-
-}
+    };
 
 var ViewModel = function() {
-
     var self = this;
-
     //Create location list to be an observable array.
     this.locationList = ko.observableArray([]);
 
@@ -75,7 +70,7 @@ var ViewModel = function() {
     });
 
     //Filter list
-    this.filterTerm = ko.observable("");
+    this.filterTerm = ko.observable('');
 
     self.filteredList = ko.computed(function(){
         if (!self.filterTerm()) {
@@ -83,7 +78,9 @@ var ViewModel = function() {
                 markerlist[i].setVisible(true);
             }
          return self.locationList();
-        } else {
+        }
+        else
+        {
         return ko.utils.arrayFilter(self.locationList(), function(loc){
             if (loc.name.toLowerCase().indexOf(self.filterTerm().toLowerCase()) >= 0) {
                 loc.locMarker.setVisible(true)
@@ -93,22 +90,26 @@ var ViewModel = function() {
                 loc.locMarker.setVisible(false);
                 //markerlist[loc.markerindex()].setVisible(false)
                 return false;
-            }
-        });
-    };
-});
-
+                    }
+            });
+        };
+    });
 
     //Function to handle when users clicks on filterd location. Data-bind on click for the filtered location
     this.clickedLocation = function(loc) {
-        var selected_result = resultList[loc.markerindex()];
-        //console.log(selected_result)
-
+        var counter = 0;
+        name = loc.name;
+        for (var i = 0; i < resultList.length; i++) {
+            if (resultList[i].name === name) {
+                counter = i;
+            }
+        }
+        var selected_result = resultList[counter];
         if (selected_result) {
             toggleBounce(loc.locMarker);
         }
         fillcontent(selected_result, loc.locMarker);
-};
+    };
 
     //Function to handle when users clicks on filterd walmart location.
     this.walmartclickedLocation = function(loc) {
@@ -121,16 +122,16 @@ var ViewModel = function() {
             if (walmartMarkerslist[i].id == id_number) {
                 walmartmarker = counter;
             }
-        else{
-            counter++;
+            else{
+                counter++;
+            }
         }
-    }
         var selected_walmartmarker = walmartMarkerslist[walmartmarker];
         if (selected_walmartmarker) {
             toggleBounce(selected_walmartmarker);
-        }
-        walmartcontent(selected_walmartmarker);
-};
+            }
+            walmartcontent(selected_walmartmarker);
+    };
 
     //function to open and close thumbnail nav
     this.openMenu = function() {
@@ -145,7 +146,7 @@ var ViewModel = function() {
                 document.getElementById("nav").style.position = "relative";
                 menu ("open");
             }
-        };
+    };
 
     //Data for Walmart
     this.walmartlist = ko.observableArray([]);
@@ -160,7 +161,8 @@ var ViewModel = function() {
                 walmartMarkerslist[i].setVisible(true);
             }
             return self.walmartlist();
-        }else {
+        }
+        else {
         return ko.utils.arrayFilter(self.walmartlist(), function(loc){
             if (loc.title().toLowerCase().indexOf(self.walmartFilterTerm().toLowerCase()) >=0) {
                 for (var i = 0; i < walmartMarkerslist.length; i++) {
@@ -172,22 +174,22 @@ var ViewModel = function() {
                     }
                 }
                 return true;
-            }else {
+            }
+            else {
                 return false;
             }
             });
         };
     });
 
-//Make zipCode to be ko.observable.
-self.zipCode = ko.observable('');
+    //Make zipCode to be ko.observable.
+    self.zipCode = ko.observable('');
 
-//Run navigate function to get users location.
-navigate()
+    //Run navigate function to get users location.
+    navigate()
 
-// When submit button is clicked, run the ajax asyn
-self.getLocations =function() {
-
+    // When submit button is clicked, run the ajax asyn
+    self.getLocations =function() {
     //validate users zip code entry
     if ((isValidUSZip(self.zipCode()) == false)&&(self.zipCode() != ''))
          {alert("Please provide a valid US zip"); return }
@@ -240,22 +242,22 @@ self.getLocations =function() {
                         })
 
                     };
-                    //console.log(walmartMarkerslist[0])
-                        walmartMarkerslist.forEach(function(loc){
-                        self.walmartlist.push(new walmartdata(loc));
-                    });
-
-                    var bounds = new google.maps.LatLngBounds();
-                    for (var i = 0; i < allWarmartlatlng.length; i++ ) {
-                    bounds.extend (allWarmartlatlng[i]);
-                    }
-                    map.fitBounds(bounds);
-                }).fail(function(){
-                    alert('Walmart cannot provide any information at the moment')
+                //console.log(walmartMarkerslist[0])
+                walmartMarkerslist.forEach(function(loc){
+                        self.walmartlist.push(new walmartData(loc));
                 });
-    return false;
+
+                var bounds = new google.maps.LatLngBounds();
+                for (var i = 0; i < allWarmartlatlng.length; i++ ) {
+                bounds.extend (allWarmartlatlng[i]);
+                }
+                map.fitBounds(bounds);
+            }).fail(function(){
+                    alert('Walmart cannot provide any information at the moment')
+            });
+        return false;
+        };
     };
-};
 
 var vm = new ViewModel();
 ko.applyBindings(vm);
@@ -315,32 +317,33 @@ function initMap() {
                         '&client_id=' +  clientID + '&client_secret=' + clientSecret +
                         '&v=20161212' + '&query=' + locMarker.name;
 
-                    $.getJSON(foursquareUrl).done(function(data) {
 
-                        var results = data.response.venues
-                        var result = results[0]
-                        //push each result into an array
-                        resultList.push(result)
+    $.getJSON(foursquareUrl).done(function(data) {
+        var results = data.response.venues
+        var result = results[0]
+        //push each result into an array
+        resultList.push(result)
 
-                        //Toggle marker and open infowindow on click
-                        locMarker.addListener('click',function(){
-                        toggleBounce(locMarker);
-                        fillcontent(result,locMarker);
-                    })
-                        //put all lat and lng in an array
-                        allLatlng.push(locMarker.position)
 
-                        var bounds = new google.maps.LatLngBounds();
-                        for (var i = 0; i < allLatlng.length; i++ ) {
-                        bounds.extend (allLatlng[i]);
-                        }
-                        map.fitBounds(bounds);
-
-                    }).fail(function(){
-                        alert("There was an error with the API call to Foursquare. Refresh and try again.")
-                    });
+        //Toggle marker and open infowindow on click
+        locMarker.addListener('click',function(){
+        toggleBounce(locMarker);
+        fillcontent(result,locMarker);
         })
-    };
+        //put all lat and lng in an array
+        allLatlng.push(locMarker.position)
+
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < allLatlng.length; i++ ) {
+        bounds.extend (allLatlng[i]);
+        }
+        map.fitBounds(bounds);
+
+        }).fail(function(){
+        alert("There was an error with the API call to Foursquare. Refresh and try again.")
+        });
+    })
+};
 
 
 //Function to toggle each location when clicked
@@ -348,34 +351,48 @@ function toggleBounce(marker) {
     if (marker.getAnimation() !== null) {
       marker.setAnimation(null);
     } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function(){
         marker.setAnimation(null);
-      }, 3000);
-    }
-  };
+        }, 1400);
+        }
+    };
 
 //Function to fill the selected location marker with HTML content.
 function fillcontent(selected_result,marker) {
+    var address, city, state, phone, url;
+    !!selected_result.location.address ? address = selected_result.location.address : address = "No address available";
+    !!selected_result.location.city ? city = selected_result.location.city : city = "city not available";
+    !!selected_result.location.state ? state = selected_result.location.state : city = "state is not available";
+    !!selected_result.contact.formattedPhone ? phone = selected_result.contact.formattedPhone : phone = "No phone number provided";
+    !!selected_result.url ? url = selected_result.url : city = "No url provided";
+
     var html = '<div class="markerClass">' +
                 '<h1>' + selected_result.name + '</h1>' +
-                '<h3>' + selected_result.location.address + ', ' +  selected_result.location.city + ', ' +
+                '<h3>' + address + ', ' +  city + ', ' +
                 selected_result.location.state + '.' + '</h3>' +
-                '<p>' + 'phoneNumber: ' + selected_result.contact.formattedPhone + '</p>' + '<br>' +
-                'url: ' + selected_result.url
+                '<p>' + 'phoneNumber: ' + phone + '</p>' + '<br>' +
+                'url: ' + url
         infowindow.setContent(html);
         infowindow.open(map, marker);
-        }
+    }
 
 //Function to fill the selected walmart location marker with HTML content.
 function walmartcontent(marker) {
+    var title, address, city, state, zip, phone, openSun, id;
+    !!marker.title ? title = marker.title : title = "No info provided on title";
+    !!marker.address ? address = marker.address : city = "No address available";
+    !!marker.openSundays ? openSun = marker.openSundays : openSun = "No info on Sunday hours";
+    !!marker.phone ? phone = marker.phone : phone = "No phone number provided";
+    !!marker.id ? id = marker.id : city = "No id provided";
+
     var html =
     '<div class="markerClass">' +
     '<h1>' + marker.title + '</h1>' +
-    '<h3>' + marker.address + ', ' +  marker.city + ', ' + marker.state + ' ' + marker.zip + '</h3>' +
-    '<br/>' + marker.phone +
-    '<p>' + 'OpenSundays: ' + marker.openSundays + '</p>' +
-    'Walmart id: ' + marker.id +
+    '<h3>' + address + ', ' +  marker.city + ', ' + marker.state + ' ' + marker.zip + '</h3>' +
+    '<br/>' + phone +
+    '<p>' + 'OpenSundays: ' + openSun + '</p>' +
+    'Walmart id: ' + id +
     '</div>'
     infowindow.setContent(html);
     infowindow.open(map, marker);
